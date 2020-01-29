c1d2fc63264dceec123ca872d68d9bc35aad8645
Ginkishi <vicmarvu98@gmail.com>
2020-01-29 14:17:31 +0100

    Modification du design v3

--- TestMenu/script2.js
+++ TestMenu/script2.js
@@ -1,24 +1,13 @@
 var toggleBtn = document.getElementById("toggleBtn");
 var sidebar = document.querySelector(".sidebar");
-var sidebarItems = document.querySelectorAll(".sidebar-item");
+var sidebarItems = document.querySelectorAll(".sidebar-item.drop");
 toggleBtn.addEventListener("click", () => {
   sidebar.classList.toggle("show");
 });
 sidebarItems.forEach(item => {
   item.addEventListener("click", () => {
-    if (item.querySelector(".submenu") != null) {
-      //   item.querySelector(".submenu").classList.toggle("show");
-      if (item.querySelector(".submenu").classList.contains("show")) {
-        item.querySelector(".submenu").classList.toggle("show");
-      } else {
-        let current = document.querySelector(".submenu.show");
-        if (current != null) {
-          current.classList.toggle("show");
-          item.querySelector(".submenu").classList.toggle("show");
-        } else {
-          item.querySelector(".submenu").classList.toggle("show");
-        }
-      }
-    }
+    console.log(item);
+
+    item.classList.toggle("show");
   });
 });
