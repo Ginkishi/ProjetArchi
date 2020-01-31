a494bda8b2c510f7a0863cd8f2ce6251138913cb
Ginkishi <vicmarvu98@gmail.com>
2020-01-31 19:08:44 +0100

    Modification de index.php pour permettre la redirect

--- app/index.php
+++ app/index.php
@@ -1,37 +1,36 @@
 <?php
-	require_once("config.php");
+require_once("config.php");
 
-	error_reporting(E_STRICT | E_ALL);
-	date_default_timezone_set('Europe/Paris');
-	setlocale (LC_TIME, 'fr_FR.utf8','fra');
+error_reporting(E_STRICT | E_ALL);
+date_default_timezone_set('Europe/Paris');
+setlocale(LC_TIME, 'fr_FR.utf8', 'fra');
 
-	// =====================  Détermination du controleur à utiliser: Est-ce que j'ai un paramètre 'c' dans mon URL?
-	if (isset($_GET['c'])){
-		//Il y a un paramètre de précisé: c'est le nom du controleur demandé.
-		$controller=strtolower(trim($_GET['c']));
-	}else{
-		//Pas de paramètre => le contrôleur par défaut est le contrôleur HOME
-		$controller='login';
-	}
+// =====================  Détermination du controleur à utiliser: Est-ce que j'ai un paramètre 'c' dans mon URL?
+if (isset($_GET['c']) && !empty($_GET['c'])) {
+	//Il y a un paramètre de précisé: c'est le nom du controleur demandé.
+	$controller = strtolower(trim($_GET['c']));
+} else {
+	//Pas de paramètre => le contrôleur par défaut est le contrôleur HOME
+	$controller = 'login';
+}
 
-	// =====================  Détermination de la méthode à appeler: Est-ce que j'ai un paramètre 'm' dans mon URL?
-	if (isset($_GET['m'])){
-			//Il y a un paramètre de précisé: c'est le nom de la méthode demandée.
-	$method=strtolower(trim($_GET['m']));
-	}else{
-		//Pas de paramètre => la méthode par défaut est la méthode INDEX
-		$method='index';
-	}
+// =====================  Détermination de la méthode à appeler: Est-ce que j'ai un paramètre 'm' dans mon URL?
+if (isset($_GET['m']) && !empty($_GET['m'])) {
+	//Il y a un paramètre de précisé: c'est le nom de la méthode demandée.
+	$method = strtolower(trim($_GET['m']));
+} else {
+	//Pas de paramètre => la méthode par défaut est la méthode INDEX
+	$method = 'index';
+}
 
-	// =====================  Appel
-	//On construit le nom du fichier qui contient le contrôleur appelé (ou le contrôleur par défaut)
-	$controllerfilename=$controller.'C.php';
-	//On inclut les fichiers nécessaires
-	include CONTROLLERS.DS.$controllerfilename;
-	//On construit le nom de la classe que l'on va instancier
-	$controllerclassname=ucfirst($controller).'Controller';
-	//On instancie cette classe
-	$c=new $controllerclassname();
-	//On appelle la méthode demandée (ou la méthode par défaut)
-	$c->$method();
-?>
+// =====================  Appel
+//On construit le nom du fichier qui contient le contrôleur appelé (ou le contrôleur par défaut)
+$controllerfilename = $controller . 'C.php';
+//On inclut les fichiers nécessaires
+include CONTROLLERS . DS . $controllerfilename;
+//On construit le nom de la classe que l'on va instancier
+$controllerclassname = ucfirst($controller) . 'Controller';
+//On instancie cette classe
+$c = new $controllerclassname();
+//On appelle la méthode demandée (ou la méthode par défaut)
+$c->$method();
