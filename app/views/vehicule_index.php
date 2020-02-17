7b33d160d910aa99a6b8628bc56ba72e92b669c9
Ginkishi <vicmarvu98@gmail.com>
2020-02-17 12:07:47 +0100

    Modification affichage véhicule

--- app/views/vehicule_index.php
+++ app/views/vehicule_index.php
@@ -1,22 +1,28 @@
-<?php
+<div class="vehicule-container">
+    <?php
 	// var_dump($_SERVER);
 
 	$id = 0;
 	$i = 0;
+
 	foreach ($records as $r) {
 		if ($r["V_ID"] != $id) {
-			echo "<br>";
+
+			if ($id != 0 && $r["V_ID"] != $id) {
+				echo "</ul></div></div>";
+			}
 			$id = $r["V_ID"];
 			$i++;
-			echo "Véhicule " . $i . " : <br>";
-			echo "Modèle du véhicule : " . $r["V_MODELE"] . "<br>";
-			echo "Nom du véhicule : " . $r["V_INDICATIF"] . "<br>";
-			echo "Immatriculation : " . $r["V_IMMATRICULATION"] . "<br>";
-			echo "Kilomètrages : " . $r["V_KM"] . "<br>";
-			echo "Composition :<br>";
-			echo utf8_encode($r["ROLE_NAME"]) . "<br>";
+			echo '<div class="vehicule-card"><div class="logo"><i class="fa fa-car fa-3x"></i></div>';
+			echo '<a href="#"><div class="title">' . $r["V_INDICATIF"] . '</div></a>';
+			echo '<div class="model"><span class="label">Modèle : </span>' . $r["V_MODELE"] . '</div>';
+			echo '<div class="imma"><span class="label">Immatriculation : </span>' . $r["V_IMMATRICULATION"] . '</div>';
+			echo '<div class="km"><span class="label">Kilometrage : </span>' . $r["V_KM"] . '</div>';
+			echo '<div class="compo"><span class="label">Composition : </span><ul>';
+			echo '<li>' . utf8_encode($r["ROLE_NAME"]) . '</li>';
 		} else {
-			echo utf8_encode($r["ROLE_NAME"]) . "<br>";
+			echo '<li>' . utf8_encode($r["ROLE_NAME"]) . '</li>';
 		}
 	}
-?>
+	?>
+</div>
