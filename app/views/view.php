6ba7887d080abc7e0487a709da0f95a7acb4ce1b
MONNET Quentin <psyrkoz68@gmail.com>
2020-02-17 14:04:41 +0100

    Modificatinon View.php pour gerer la page de login qui est différente
    Légère modification de View.php pour inclure les bons fichier lors de la demande d'affichage du Login (qui a un layout différent)

--- app/views/view.php
+++ app/views/view.php
@@ -14,16 +14,28 @@
         public function afficher($displayfile)
         {
             extract($this->listevar);
+
+
             echo '<!doctype html>';
 			echo '<html lang="fr">';
 			echo '<head>';
 			include VIEWS . DS . 'common' . DS . 'head.php';
-			echo '</head>';
-			echo '<body>';
-			include VIEWS . DS . 'common' . DS . 'nav.php';
-			include VIEWS . DS . strtolower($displayfile) . ".php";
-			include VIEWS . DS . 'common' . DS . 'bs_js.php';
 
+            if($displayfile == "login_index")
+            {
+                echo '<link href="./views/assets/css/login.css" rel="stylesheet">';
+                echo '<link href="./views/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">';
+                echo '</head>';
+                echo '<body>';
+			    include VIEWS . DS . $displayfile . ".php";
+            }
+            else
+            {
+                include VIEWS . DS . 'common' . DS . 'nav.php';
+                include VIEWS . DS . strtolower($displayfile) . ".php";
+            }
+
+            include VIEWS . DS . 'common' . DS . 'bs_js.php';
 			echo '</body></html>';
         }
     }
