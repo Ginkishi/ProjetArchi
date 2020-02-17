4c3386b753a10950f557cd18f09ce545c61342c2
MONNET Quentin <psyrkoz68@gmail.com>
2020-02-17 15:19:48 +0100

    Ajout méthode pour afficher le Login
    Le Login utilisant une structure différentes de page (il n'y a pas de nav bar et il faut un css spécial), l'ajout d'une fonction pour l'afficher a été nécessaire.

--- app/views/view.php
+++ app/views/view.php
@@ -14,27 +14,31 @@
         public function afficher($displayfile)
         {
             extract($this->listevar);
-
-
             echo '<!doctype html>';
 			echo '<html lang="fr">';
 			echo '<head>';
 			include VIEWS . DS . 'common' . DS . 'head.php';
+            echo "</head><body>";
+            include VIEWS . DS . 'common' . DS . 'nav.php';
+            include VIEWS . DS . strtolower($displayfile) . ".php";
+            include VIEWS . DS . 'common' . DS . 'bs_js.php';
+			echo '</body></html>';
+        }
 
-            if($displayfile == "login_index")
-            {
-                echo '<link href="./views/assets/css/login.css" rel="stylesheet">';
-                echo '<link href="./views/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">';
-                echo '</head>';
-                echo '<body>';
-			    include VIEWS . DS . $displayfile . ".php";
-            }
-            else
-            {
-                include VIEWS . DS . 'common' . DS . 'nav.php';
-                include VIEWS . DS . strtolower($displayfile) . ".php";
-            }
-
+        public function afficherLogin()
+        {
+            extract($this->listevar);
+            echo '<!doctype html>';
+			echo '<html lang="fr">';
+			echo '<head>';
+			include VIEWS . DS . 'common' . DS . 'head.php';
+            echo '<link href="./views/assets/css/login.css" rel="stylesheet">';
+            echo '<link href="./views/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">';
+            
+            echo '<link href="../views/assets/css/login.css" rel="stylesheet">';
+            echo '<link href="../views/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">';
+            echo '</head><body>';
+            include VIEWS . DS . 'login_index.php';
             include VIEWS . DS . 'common' . DS . 'bs_js.php';
 			echo '</body></html>';
         }
