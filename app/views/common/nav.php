092897feef92d2cae00ea4eaa032d108a649fe7b
Ginkishi <vicmarvu98@gmail.com>
2020-02-17 12:35:10 +0100

    Changement de menu

--- app/views/common/nav.php
+++ app/views/common/nav.php
@@ -1,31 +1,28 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">UHA MIAGE/IM</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="<?= LOCAL_DIR ?>home/index">Accueil<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="<?= LOCAL_DIR ?>intervention/add">Ajouter une intervention</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="<?= LOCAL_DIR ?>vehicule">Liste des véhicules</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="<?= LOCAL_DIR ?>login/disconnect">Déconnexion</a>
-            </li>
-            <!--<li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>-->
-        </ul>
+<div class="menu">
+    <div id="toggleBtn" class="toggleBtn"><i class="fa fa-bars fa-2x"></i></div>
+</div>
+<div class="sidebar show" id="sidebar">
+    <div class="photo">
+        <i class="fa fa-user-circle fa-4x"></i>
     </div>
-</nav>
+    <div class="info"><span class="nom">Victor MARFIL</span><span class="role">Administrator</span></div>
+    <div class="tool">
+        <a href="#" class="notif"><i class="fa fa-bell "></i></a>
+        <a href="#" class="msg"><i class="fa fa-envelope"></i></a>
+        <a href="#" class="setting"><i class="fa fa-cog"></i></a>
+        <a href="<?= LOCAL_DIR ?>login/disconnect" class="statut"><i class="fa fa-power-off"></i></a>
+    </div>
+    <div class="sidebar-container">
+        <a href="<?= LOCAL_DIR ?>home/index" class="sidebar-item"><span class="title-item"><i class="fa fa-home fa-2x"></i>Accueil</span></a>
+        <div class="sidebar-item drop">
+            <span class="title-item"><i class="fa fa-map-marker fa-2x"></i>Intervention</span>
+            <div class="submenu">
+                <a href="/test" class="submenu-item"><span class="title-item"><i class="fa fa-list fa-2x"></i>Liste</span></a>
+                <a href="<?= LOCAL_DIR ?>intervention/add" class="submenu-item"><span class="title-item"><i class="fa fa-plus-circle fa-2x"></i>Ajouter</span></a>
+            </div>
+        </div>
+        <a href="#" class="sidebar-item"><span class="title-item"><i class="fa fa-user fa-2x"></i>Pompier</span></a>
+        <a href="<?= LOCAL_DIR ?>vehicule/index" class="sidebar-item"><span class="title-item"><i class="fa fa-car fa-2x"></i>Véhicules</span></a>
+        <div class="sidebar-item"><span class="title-item"><i class="fa fa-id-badge fa-2x"></i>Roles</span></div>
+    </div>
+</div>
