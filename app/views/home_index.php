d7c94e843cacca3a29295da2e29449644c6ef94b
Quentin Monnet <psyrkoz68@gmail.com>
2020-02-18 18:52:46 +0100

    Modification de la page d'accueil
    Modification de la page d'accueil pour utiliser la nouvelle classe de gestion de session.

--- app/views/home_index.php
+++ app/views/home_index.php
@@ -2,22 +2,11 @@
     <div class="starter-template">
         <h1>Page d'accueil par défaut</h1>
         <?php
-            
-
-			if (version_compare(phpversion(), '5.4.0', '<')) {
-				 if(session_id() == '') {
-					session_start();
-				 }
-			 }
-			 else
-			 {
-				if (session_status() == PHP_SESSION_NONE) {
-					session_start();
-				}
-			 }
-
-
-            if(isset($_SESSION['nom']) && isset($_SESSION['prenom']) && isset($_SESSION['grade']))
+            require_once(CONTROLLERS.DS."sessionHandler.php");
+            require_once(VIEWS.DS."view.php");
+			GestionnaireSession::ouvreSession();
+			
+            if(GestionnaireSession::is_set())
             {
                 echo "<p class='lead'>Bonjour ".$_SESSION["grade"]." ".$_SESSION['nom']." ".$_SESSION['prenom']."</p>";
             }
@@ -25,7 +14,7 @@
 			{
 				$host = $_SERVER['HTTP_HOST'] . DS;
 				$uri = "projetarchi" . DS . ROOT_DIR . "/login";
-				$extra = "/disconnect";
+				$extra = "";
 
 				header("Location: http://$host$uri$extra");
 			}
