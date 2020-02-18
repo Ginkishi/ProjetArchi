910ab46bc12472f67ee63e29b69aad6a95cb434f
Lilian <lilian.miesiac@gmail.com>
2020-02-18 17:29:44 +0100

    Create sessionHandler.php

---
+++ app/controllers/sessionHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+	class SessionHandler
+	{
+		public function __construct()
+		{
+		}
+
+		public static function ouvreSession()
+		{
+		}
+
+		public static function is_set()
+		{
+		}
+		
+		public static function un_set()
+		{
+		}
+		
+		public static function aLesRoles(...$roles)
+		{
+		}
+	}
+?>
