ca5b7174cf3a6174a6a3c76ab1026ab54ed8632a
Lilian <lilian.miesiac@gmail.com>
2020-02-18 18:37:50 +0100

    Update loginC.php

--- app/controllers/loginC.php
+++ app/controllers/loginC.php
@@ -2,6 +2,7 @@
 	require_once(VIEWS.DS."view.php");
 	require_once(MODELS.DS."loginM.php");
 	require_once("./controllers/homeC.php");
+	require_once("./controllers/sessionHandler.php");
 
 	class LoginController
 	{
@@ -12,21 +13,11 @@
 		public function index()
 		{
 			// Check if PHP session is started (start it if not already started)
-			if (version_compare(phpversion(), '5.4.0', '<')) {
-				if(session_id() == '') {
-				   session_start();
-			   	}
-		   	}
-		   	else
-		   	{
-			   if (session_status() == PHP_SESSION_NONE) {
-				   session_start();
-			   	}
-		   	}
+			GestionnaireSession::ouvreSession();
 
 		   	// Si deja connecter -> redirection vers la home page
 		   	$v = new View();
-		   	if(isset($_SESSION) && isset($_SESSION["nom"]) && !empty($_SESSION["nom"]) && isset($_SESSION["prenom"]) && !empty($_SESSION["prenom"]) && isset($_SESSION["grade"]) && !empty($_SESSION["grade"]))
+		   	if(GestionnaireSession::is_set())
 		   	{
 			   $v->afficher("home_index");
 			}
@@ -38,28 +29,23 @@
 
 		public function authenticate()
 		{
-			if (version_compare(phpversion(), '5.4.0', '<')) {
-				if(session_id() == '') {
-					session_start();
-				}
-			}
-			else
-			{
-				if (session_status() == PHP_SESSION_NONE) {
-					session_start();
-				}
-			}
+			GestionnaireSession::ouvreSession();
 
 			$modele = new LoginModel();
 			$record = $modele->connect($_POST["username"], $_POST["password"]);
 
 			if (sizeof($record) == 1) {
 				//var_dump($record[0]);
-				$_SESSION["id"] = $record[0]['P_ID'];
+				GestionnaireSession::initializeSession($record[0]['P_ID'],
+													   $record[0]['P_CODE'],
+													   $record[0]['P_NOM'],
+													   $record[0]['P_PRENOM'],
+													   $record[0]['P_GRADE']);
+				/**$_SESSION["id"] = $record[0]['P_ID'];
 				$_SESSION["code"] = $record[0]['P_CODE'];
 				$_SESSION["nom"] = $record[0]['P_NOM'];
 				$_SESSION["prenom"] = $record[0]['P_PRENOM'];
-				$_SESSION["grade"] = $record[0]['P_GRADE'];
+				$_SESSION["grade"] = $record[0]['P_GRADE'];*/
 
 				$v = new View();
 				$v->afficher("home_index");
@@ -72,15 +58,15 @@
 		
 		public function disconnect()
 		{
-			session_start();
+			GestionnaireSession::ouvreSession();
 
 			$v = new View();
-			if(isset($_SESSION) && isset($_SESSION["id"]) && isset($_SESSION["code"]) && isset($_SESSION["nom"]) && isset($_SESSION["prenom"]) && isset($_SESSION["grade"]))
+			if(GestionnaireSession::is_set())
 			{
 				$v->ajouterVariable("sucess_message", "Vous avez bien été déconnecté!");
-				unset($_SESSION);
+				GestionnaireSession::un_set();
 			}		
-			session_destroy();
+			GestionnaireSession::detruire();
 			$v->afficherLogin();
 		}
 	}
