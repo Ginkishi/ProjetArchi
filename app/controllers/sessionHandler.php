0a8bc6b8d6c5494a8e51542062b660a8e5dc135f
Lilian <lilian.miesiac@gmail.com>
2020-02-18 17:34:49 +0100

    Update sessionHandler.php

--- app/controllers/sessionHandler.php
+++ app/controllers/sessionHandler.php
@@ -8,18 +8,37 @@
 
 		public static function ouvreSession()
 		{
+			if (version_compare(phpversion(), '5.4.0', '<')) {
+				if(session_id() == '') {
+				   session_start();
+			   	}
+		   	}
+		   	else
+		   	{
+			   if (session_status() == PHP_SESSION_NONE) {
+				   session_start();
+			   	}
+		   	}
 		}
 
 		public static function is_set()
 		{
+			return isset($_SESSION) && isset($_SESSION["nom"]) && !empty($_SESSION["nom"]) && isset($_SESSION["prenom"]) && !empty($_SESSION["prenom"]) && isset($_SESSION["grade"]) && !empty($_SESSION["grade"])
 		}
 		
 		public static function un_set()
 		{
+			unset($_SESSION);
+		}
+		
+		public static function detruire()
+		{
+			session_destroy();
 		}
 		
 		public static function aLesRoles(...$roles)
 		{
+			var_dump($roles);
 		}
 	}
 ?>
