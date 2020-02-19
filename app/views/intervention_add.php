06f88a29c7e10b175c7aecd60915c02468579334
Ginkishi <vicmarvu98@gmail.com>
2020-02-19 15:03:02 +0100

    Rajout du nom pour chaque input

--- app/views/intervention_add.php
+++ app/views/intervention_add.php
@@ -7,65 +7,65 @@
                 <div class="group-champ col3">
                     <div class="champ">
                         <label for="">Numéro d'intervention</label>
-                        <input type="text" autocomplete="off">
+                        <input type="text" autocomplete="off" name="numIntervention" required>
                         <div class="barre"></div>
                     </div>
                     <div class="champ">
                         <label for="">Commune</label>
-                        <input type="text" autocomplete="off">
+                        <input type="text" autocomplete="off" name="commune" required>
                         <div class="barre"></div>
                     </div>
                     <div class="champ">
                         <label for="">Adresse</label>
-                        <input type="text" autocomplete="off">
+                        <input type="text" autocomplete="off" name="adresse" required>
                         <div class="barre"></div>
                     </div>
                 </div>
                 <div class="champ">
                     <label for="">Type d'intervention</label>
-                    <select name="" id="" class="form-control">
+                    <select name="typeIntervention" id="typeIntervention" class="form-control">
                         <option value="">Selectionnez un type d'intervention</option>
                     </select>
                 </div>
                 <div class="group-champ col2">
                     <div class="champ">
                         <label for="">Réquerant</label>
-                        <select name="" id="" class="form-control">
+                        <select name="requerant" id="requerant" class="form-control">
                             <option value="">CODIS</option>
                         </select>
                     </div>
                     <div class="group-champ col2">
                         <div class="champ mycheckbox">
                             <label for="">OPM</label>
-                            <input type="checkbox">
+                            <input type="checkbox" name="opm" id="opm">
                         </div>
                         <div class="champ mycheckbox">
                             <label for="">Important</label>
-                            <input type="checkbox">
+                            <input type="checkbox" name="important" id="important">
                         </div>
                     </div>
                 </div>
                 <div class="group-champ col2">
                     <div class="champ">
                         <label for="">Date déclenchement</label>
-                        <input type="date" autocomplete="off">
+                        <input type="date" autocomplete="off" name="dateDeclenchement">
                         <div class="barre"></div>
                     </div>
                     <div class="champ">
                         <label for="">Heure déclenchement</label>
-                        <input type="time" autocomplete="off">
+                        <input type="time" autocomplete="off" name="heureDeclenchement">
                         <div class="barre"></div>
                     </div>
                 </div>
                 <div class="group-champ col2">
                     <div class="champ">
                         <label for="">Date de fin</label>
-                        <input type="date" autocomplete="off">
+                        <input type="date" autocomplete="off" name="dateFin">
                         <div class="barre"></div>
                     </div>
                     <div class="champ">
                         <label for="">Heure de fin</label>
-                        <input type="time" autocomplete="off">
+                        <input type="time" autocomplete="off" name="heureFin">
                         <div class="barre"></div>
                     </div>
                 </div>
@@ -75,44 +75,44 @@
             <h2 class="title">Engins et Personnels</h2>
             <div class="body">
                 <div class="champ">
-                    <label for="">Nom du responsable</label>
-                    <select name="" id="" class="form-control">
+                    <label for="">Nom du v&eacute;hicule</label>
+                    <select name="typeEngin[]" id="nomEngin%0" class="form-control">
                         <option value="">Selectionnez un véhicule</option>
                     </select>
                 </div>
                 <div class="group-champ col2">
                     <div class="champ">
                         <label for="">Date de départ</label>
-                        <input type="date" autocomplete="off">
+                        <input type="date" autocomplete="off" name="dateDepart[]">
                         <div class="barre"></div>
                     </div>
                     <div class="champ">
                         <label for="">Heure de départ</label>
-                        <input type="time" autocomplete="off">
+                        <input type="time" autocomplete="off" name="heureDepart[]">
                         <div class="barre"></div>
                     </div>
                 </div>
                 <div class="group-champ col2">
                     <div class="champ">
                         <label for="">Date d'arrivée sur les lieux</label>
-                        <input type="date" autocomplete="off">
+                        <input type="date" autocomplete="off" name="dateArrivee[]">
                         <div class="barre"></div>
                     </div>
                     <div class="champ">
                         <label for="">Heure d'arrivée sur les lieux</label>
-                        <input type="time" autocomplete="off">
+                        <input type="time" autocomplete="off" name="heureArrivee[]">
                         <div class="barre"></div>
                     </div>
                 </div>
                 <div class="group-champ col2">
                     <div class="champ">
                         <label for="">Date de retour</label>
-                        <input type="date" autocomplete="off">
+                        <input type="date" autocomplete="off" name="dateRetour[]">
                         <div class="barre"></div>
                     </div>
                     <div class="champ">
                         <label for="">Heure de retour</label>
-                        <input type="time" autocomplete="off">
+                        <input type="time" autocomplete="off" name="heureRetour[]">
                         <div class="barre"></div>
                     </div>
                 </div>
@@ -124,7 +124,7 @@
             <div class="body">
                 <div class="champ">
                     <label for="">Nom du responsable</label>
-                    <input type="text" autocomplete="off">
+                    <input type="text" autocomplete="off" name="responsable">
                     <div class="barre"></div>
                 </div>
             </div>
