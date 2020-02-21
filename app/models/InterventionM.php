60b33feb0a5957b217bde7c43e7e9a9f013239ed
Yasmine <yasminehad@hotmail.com>
2020-02-21 13:50:19 +0100

    update ajout d'intervention

--- app/models/InterventionM.php
+++ app/models/InterventionM.php
@@ -13,7 +13,7 @@
 		}
 
 
-		public function AddIntervention($numIntervention, $adresse, $commune,$opm,$typeIntervention,$important,$requerant,$dateDeclenchement,$heureDeclenchement,$dateFin,$heureFin,$responsable,$idcreateur)
+		public function AddIntervention($numIntervention, $adresse, $commune,$opm,$typeIntervention,$important,$requerant,$dateDeclenchement,$heureDeclenchement,$dateFin,$heureFin,$responsable,$idcreateur, $status)
 		{  
 
 		 //  echo   $numIntervention."<br>".$adresse."<br>".$commune."<br>".$typeIntervention."<br>".$dateDeclenchement."<br>".$heureDeclenchement."<br>";
@@ -34,7 +34,7 @@
 		  /* echo $idresp;
 		   echo $type;
 		   echo "fin";*/ 
-	      $this->con->query("INSERT INTO  interventions (NIntervention, OPM, Commune, Adresse, TypeIntervention, Important, Requerant, DateDeclenchement, DateFin, IDResponsable, IDCreateur) VALUES($numIntervention,$opm,'$commune','$adresse', '$type',$important,'$requerant','$datedec','$datef',$idresp,$idcreateur);");
+	      $this->con->query("INSERT INTO  interventions (NIntervention, OPM, Commune, Adresse, TypeIntervention, Important, Requerant, DateDeclenchement, DateFin, IDResponsable, IDCreateur,IDstatus) VALUES($numIntervention,$opm,'$commune','$adresse', '$type',$important,'$requerant','$datedec','$datef',$idresp,$idcreateur, $status);");
 			//echo
 			$query=$this->con->query("SELECT IDIntervention FROM  interventions where NIntervention=$numIntervention AND DateDeclenchement='$datedec'");
 			$ID = $query->fetch();
