3b47e71404e1a59b20e0be4c7f6328c92eb39285
Ginkishi <vicmarvu98@gmail.com>
2020-03-11 19:49:30 +0100

    Restructuration + Ajout CSS

--- app/views/intervention_view.php
+++ app/views/intervention_view.php
@@ -4,16 +4,19 @@
         <div class="title">Intervention n°<?= $intervention["NIntervention"] ?></div>
     </div>
     <div class="body">
-        <div class="info"></div>
+        <div class="info">
+            <div id="date_dec"><span class="label">Date de d&eacute;clenchement : </span><?= $intervention["DateDeclenchement"] ?></div>
+            <div id="date_fin"><span class="label">Date de fin : </span><?= $intervention["DateFin"] ?></div>
+            <div id="adresse"><span class="label">Adresse : </span><?= $intervention["Adresse"] ?></div>
+            <div id="commune"><span class="label">Commune : </span><?= $intervention["Commune"] ?></div>
+            <div id="requerant"><span class="label">Requ&eacute;rant : </span><?= $intervention["Requerant"] ?></div>
+            <div id="responsable"><span class="label">Responsable : </span><?= $intervention["IDResponsable"] ?></div>
+        </div>
         <div class="vehicule"></div>
     </div>
     <?php
 
-    echo $intervention["DateDeclenchement"] . "<br>";
-    echo $intervention["DateFin"] . "<br>";
-    echo $intervention["Adresse"] . "<br>";
-    echo $intervention["Commune"] . "<br>";
-    echo $intervention["Requerant"] . "<br>";
+
     ?>
     <?php var_dump($vehicules) ?>
 </div>
