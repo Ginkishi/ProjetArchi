8273dd871afe33bb745645e97f463003e128fcd1
Ginkishi <vicmarvu98@gmail.com>
2020-03-13 13:07:12 +0100

    Mise en commentaire de popper.js

--- app/views/common/bs_js.php
+++ app/views/common/bs_js.php
@@ -5,7 +5,7 @@
 window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')
 </script> -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="<?= LOCAL_VENDORS ?>/bootstrap/dist/js/popper.min.js"></script>
+<!--<script src="<?= LOCAL_VENDORS ?>/bootstrap/dist/js/popper.min.js"></script>-->
 <script src="<?= LOCAL_VENDORS ?>/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="<?= LOCAL_VENDORS ?>/personal/js/cookie.js"></script>
 <script src="<?= LOCAL_VENDORS ?>/personal/js/menu.js"></script>
