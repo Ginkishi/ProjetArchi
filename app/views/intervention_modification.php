63fed8e4a79ce851ee9b064be0b030de773e27d1
Yasmine <yasminehad@hotmail.com>
2020-03-13 15:10:52 +0100

    Update intervention_modification.php
    ajout de l'autocompelete

--- app/views/intervention_modification.php
+++ app/views/intervention_modification.php
@@ -225,7 +225,7 @@
                         ?>
                                 <div id="team<?php echo $i ?>" class="champs">
                                     <label for=""><?php echo $v["vehicule"][$j]["ROLE_NAME"];?><span class="important">*</span>:</label>
-                                    <input type="text" required name="<?php echo $v["vehicule"][$j]["ROLE_NAME"];?>[]" value="<?php echo $v["vehicule"][$j]["pompier"];?>">
+                                    <input type="text" required   list="firefighters" name="<?php echo $v["vehicule"][$j]["ROLE_NAME"];?>[]" value="<?php echo $v["vehicule"][$j]["pompier"];?>">
                                 </div>
                         <?php 
                             }
